56aa6d150c4307d866497458a1b3afbfd888ad4c
Cody Dupuis <codydupuis99@gmail.com>
2020-08-05 09:47:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,10 @@
 def key_for_min_value(name_hash)
+lowest_value = 0 
   lowest_key = nil
-  lowest_value = nil
-  name_hash.collect { |key, value|
-    if lowest_value == nil || value < lowest_value
-      lowest_value = value
-      lowest_key = key
-    end
-  }
-  return lowest_key
-end
+  name_hash.each do |key, value|
+    if lowest_value == 0 || value < lowest_value
+    lowest_value = value 
+    lowest_key = key
+   end  
+  end
+    lowest_key
